f3a7a0373ae136bcf439e9900607f419f4efa664
Brian Ketelsen <bketelsen@arrakis.ketelsen.home>
2015-12-14 13:18:02 -0500

    add helper

--- model_template.go
+++ model_template.go
@@ -21,6 +21,7 @@ func {{$typeName}}FromUpdatePayload(ctx *app.Update{{demodel $typeName}}Context)
 func (m {{$typeName}}) ToApp() *app.{{demodel $typeName}} {
 	target := app.{{demodel $typeName}}{}
 	copier.Copy(&target, &m)
+	target.Href = app.{{demodel $typename}}Href(target.ID)
 	return &target 
 }
 {{ $roler := index .Metadata "github.com/bketelsen/gorma#roler" }}
