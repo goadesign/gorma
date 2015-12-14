070d0da1b2bb6584bd1c2adcda4c3ea995a7942a
Brian Ketelsen <bketelsen@arrakis.ketelsen.home>
2015-12-14 16:11:19 -0500

    trying it

--- interface_template.go
+++ interface_template.go
@@ -1,6 +1,7 @@
 package gorma
 
 const intTmpl = `
+ {{ $typeName :=  gotypename . 0}}{{$typeName := demodel $typeName}}
 type {{$typeName}}Storage interface {
 	List(ctx *app.List{{demodel $typeName}}Context) []{{$typeName}}
 	Get(ctx *app.Show{{demodel $typeName }}Context) ({{$typeName}}, error)
