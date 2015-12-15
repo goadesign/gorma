dab9c0b39fcde680c8ce721d67e110363ef9cb81
Brian Ketelsen <bketelsen@gmail.com>
2015-12-15 08:13:04 -0500

    m2m

--- model_template.go
+++ model_template.go
@@ -121,7 +121,7 @@ func (m *{{$typeName}}DB) Delete{{index $pieces 1}}(ctx *app.Delete{{$lower}}{{$
 }
 func (m *{{$typeName}}DB) Add{{index $pieces 1}}(ctx *app.Add{{$lower}}{{$typeName}}Context) error {
 	var obj {{$typeName}}
-	err := m.DB.Model(&obj).Association("{{index $pieces 0}}").Append({{index $pieces 1}}{ID: ctx.Payload.{{index $pieces 1}}Id).Error
+	err := m.DB.Model(&obj).Association("{{index $pieces 0}}").Append({{index $pieces 1}}{ID: ctx.Payload.{{index $pieces 1}}Id}).Error
 	if err != nil {
 		ctx.Logger.Error(err.Error())
 		return  err
