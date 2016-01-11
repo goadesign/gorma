148a66b56ce1220ce35801d5bc414d1b950e18f2
Brian Ketelsen <bketelsen@gmail.com>
2016-01-11 17:02:52 -0500

    oops

--- impl_template.go
+++ impl_template.go
@@ -2,6 +2,20 @@ package gorma
 
 const implTmpl = `// {{if .TypeDef.Description}}{{.TypeDef.Description}}{{else}}app.{{ .TypeName}} model type{{end}}
 // Identifier: {{ .TypeName}}
+type {{$typename}}Storage interface {
+	DB() interface{}
+	List(ctx context.Context{{ if $dynamictable }}, tableName string{{ end }}) []{{$typename}}
+	One(ctx context.Context{{ if $dynamictable }}, tableName string{{ end }}, {{ pkattributes $pks  }}) ({{$typename}}, error)
+	Add(ctx context.Context{{ if $dynamictable }}, tableName string{{ end }}, o {{$typename}}) ({{$typename}}, error)
+	Update(ctx context.Context{{ if $dynamictable }}, tableName string{{ end }}, o {{$typename}}) (error)
+	Delete(ctx context.Context{{ if $dynamictable }}, tableName string{{ end }}, {{ pkattributes $pks }}) (error)
+{{ range $idx, $bt := .BelongsTo}}
+	ListBy{{$bt.Parent}}(ctx context.Context{{ if $dynamictable }}, tableName string{{ end }}, parentid int) []{{$typename}}
+	OneBy{{$bt.Parent}}(ctx context.Context{{ if $dynamictable }}, tableName string{{ end }}, parentid, id int) ({{$typename}}, error)
+{{end}}
+	{{ storagedef $typedef }}
+}
+
 type {{.TypeName}} struct {
 	{{.ModelLower}}.{{.TypeName}}
 }
