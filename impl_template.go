d25c3e4b63ce8a950ba10dbb871b40558b199b21
Brian Ketelsen <bketelsen@gmail.com>
2016-01-11 17:33:56 -0500

    move to map for pks

--- impl_template.go
+++ impl_template.go
@@ -23,7 +23,7 @@ type {{$typename}}Storage interface {
 func New{{.TypeName}}DB(db gorm.DB) *{{.TypeName}}DB {
 	mdb := {{.ModelLower}}.New{{.TypeName}}DB(db)
 	return &{{.TypeName}}DB{
-		{{.ModelLower}}.{{TypeName}}DB: mdb
+		{{.ModelLower}}.{{.TypeName}}DB: mdb
 	}
 
 }
