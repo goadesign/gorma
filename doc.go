9e1eba238fda52fad4f68d68fe0c888d54803794
Brian Ketelsen <bketelsen@gmail.com>
2016-01-21 22:18:29 -0500

    more godoc

--- doc.go
+++ doc.go
@@ -1,1 +1,10 @@
+/*
+Gorma is a plugin generator for Goa (http://goa.design)
+See the documentation in the `dsl` package for details
+on how to create a definition for your API.
+
+The `example` folder contains an example Goa design package.
+The `models.go` file is the Gorma definition, which is responsible
+for generating all the files in the `example\models` folder.
+*/
 package gorma
