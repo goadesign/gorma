a8ffc3e3d9f48cadcb2d90db95ff3053eac426f7
Brian Ketelsen <bketelsen@gmail.com>
2016-01-22 17:21:47 -0500

    version problem with generated helpers

--- writers.go
+++ writers.go
@@ -57,10 +57,6 @@ func fieldAssignmentModelToType(model *RelationalModelDefinition, ut *design.Med
 				} else if mpointer {
 					// tfield = *mfield (rare if never?)
 					fa = fmt.Sprintf("\t%s.%s = *%s.%s", utype, codegen.Goify(key, true), mtype, fname)
-				} else {
-					// tfield = mfield
-					fa = fmt.Sprintf("\t%s.%s = %s.%s", utype, codegen.Goify(key, true), mtype, fname)
-
 				}
 				fieldAssignments = append(fieldAssignments, fa)
 
