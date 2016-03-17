dba3bc51f02f3374d48a33fbcd2d12d01c3661ac
Brian Ketelsen <bketelsen@gmail.com>
2016-03-16 20:53:33 -0400

    fix for joe

--- writers.go
+++ writers.go
@@ -105,17 +105,13 @@ func fieldAssignmentModelToType(model *RelationalModelDefinition, ut *design.Vie
 	var fieldAssignments []string
 
 	if !strings.Contains(ut.Name, "link") {
+		fmt.Println(model.ModelName, "LINK ", ut.Name)
 		for ln := range ut.Parent.Links {
-			iff := fmt.Sprintf("var tmp%dCollection app.%sLinkCollection", tmp, inflect.Singularize(codegen.Goify(ln, true)))
-			fieldAssignments = append(fieldAssignments, iff)
-			ifa := fmt.Sprintf("for _,k := range %s.%s {", v, codegen.Goify(ln, true))
-			fieldAssignments = append(fieldAssignments, ifa)
-			ifb := fmt.Sprintf("tmp%dCollection = append(tmp%dCollection,  k.%sTo%sLink())", tmp, tmp, inflect.Singularize(codegen.Goify(ln, true)), inflect.Singularize(codegen.Goify(ln, true)))
+			fmt.Println(ln)
+			ifb := fmt.Sprintf("tmp%d := %s.%s.%sTo%sLink()", tmp, v, codegen.Goify(ln, true), inflect.Singularize(codegen.Goify(ln, true)), inflect.Singularize(codegen.Goify(ln, true)))
 
 			fieldAssignments = append(fieldAssignments, ifb)
-			ifc := fmt.Sprintf("}")
-			fieldAssignments = append(fieldAssignments, ifc)
-			ifd := fmt.Sprintf("%s.Links = &app.%sLinks{%s: tmp%dCollection}", utype, codegen.Goify(utype, true), codegen.Goify(ln, true), tmp)
+			ifd := fmt.Sprintf("%s.Links = &app.%sLinks{%s: tmp%d}", utype, codegen.Goify(utype, true), codegen.Goify(ln, true), tmp)
 			fieldAssignments = append(fieldAssignments, ifd)
 			tmp++
 		}
