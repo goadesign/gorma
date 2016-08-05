d7dce6b9a3ff710663163356cb7a840ac34cf522
Brian Ketelsen <bketelsen@gmail.com>
2016-08-05 15:52:23 -0400

    remove the example folder

--- example/generate.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-goagen --design=github.com/goadesign/gorma/example/design bootstrap
-goagen --design=github.com/goadesign/gorma/example/design gen --pkg-path github.com/goadesign/gorma
