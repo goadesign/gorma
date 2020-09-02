7d7d42d783b4b9a98126ce75fa905874ce59bb98
Yasuhiro Manai <yasuhiro.manai@gmail.com>
2020-09-02 20:10:38 +0900

    Replace underscore

--- relationalmodel.go
+++ relationalmodel.go
@@ -50,7 +50,7 @@ func (f *RelationalModelDefinition) DSL() func() {
 
 // TableName returns the TableName of the struct.
 func (f RelationalModelDefinition) TableName() string {
-	return inflect.Underscore(inflection.Plural(f.ModelName))
+	return gorma.Underscore(inflection.Plural(f.ModelName))
 }
 
 // Children returns a slice of this objects children.
